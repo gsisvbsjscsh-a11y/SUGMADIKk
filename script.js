74f03abbb9214aa4420e0c8543a784d1c7e6e28f
gsisvbsjscsh-a11y <gsisvbsjscsh@gmail.com>
2025-10-22 20:15:30 -0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+// 1. Pilih tombol berdasarkan id
+const tombolsapa = document.getElementById('sapaButton');
+// 2. Tambahkan 'event listener' untuk aksi 'click'
+tombolSapa.addEventListener('click', function() {
+     // 3. Jalankan kode  ini ketika tombol di-click
+     alert('Halo! terimakasih sudah berkunjung!');
+});
